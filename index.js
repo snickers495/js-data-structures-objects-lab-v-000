7612dcd87a166c546b476d36e155889993028e2b
Lisa Lee <snickers495@gmail.com>
2018-01-12 21:58:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  return driver[key] = value
+}
