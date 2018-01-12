c700d1cf01db0244cc3088637018f86f3f14b547
Lisa Lee <snickers495@gmail.com>
2018-01-12 22:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   return driver;
 }
 function deleteFromDriverByKey(driver, key) {
-  let newDriver =  Object.assign({}, driver, { [key]: value });
+  let newDriver =  Object.assign({}, driver);
+  new
 }
